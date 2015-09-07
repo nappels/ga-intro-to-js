3fb348ab36fb6591647e96132a07ee32382881c9
Noah Appel <noah@bop.fm>
2015-09-07 14:15:01 -0700

    Including reveal.js. All class slides.

---
+++ slides/js/main.js
@@ -0,0 +1,11 @@
+var headerMap = {
+    agenda: "Agenda",
+    review: "Part 1: Intro and Review"
+};
+
+// Reveal.addEventListener( 'slidechanged', function( event ) {
+//     if (event.currentSlide.dataset.updateHeader) {
+//         var header = document.querySelector('.slide-header');
+//         header.innerHTML = headerMap[event.currentSlide.dataset.updateHeader] || "";
+//     }
+// } );
